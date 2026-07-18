d8ea01d965e942d550982c995dfce105ac67dd7c
gunamass <guna95030@gmail.com>
2026-07-18 13:35:27 +0530

    A

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
     // apiUrl: 'http://10.87.99.37:8080',
     encKey: 'WGqTO5iRgcgRXNxg',
     // apiUrl: 'http://localhost:4000',
-    apiUrl: 'https://venba-crm-node.onrender.com/',
+    apiUrl: 'https://venba-crm-node.onrender.com',
 
     // apiUrl: 'https://spring-railway-production.up.railway.app',
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     // apiUrl: 'http://10.87.99.37:8080',
     // apiUrl: 'https://spring-railway-production.up.railway.app',
     // apiUrl: 'http://localhost:4000',
-    apiUrl: 'https://venba-crm-node.onrender.com/',
+    apiUrl: 'https://venba-crm-node.onrender.com',
 };
